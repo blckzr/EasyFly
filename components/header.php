d52a5ab022cc7b6c0886b2123d736b0b6797aff9
karllogdat <logdatk@gmail.com>
2025-05-25 19:18:12 +0800

    add very unfinised navbar

---
+++ components/header.php
@@ -0,0 +1,7 @@
+<header>
+    <a href="#" class="link">Home</a>
+    <a href="#" class="link">Book</a>
+    <a href="#" class="link">View Bookings</a>
+    <a href="#" class="link">About</a>
+    <a href="#"><img href="#"></a>
+</header>
