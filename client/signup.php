e37986b077d5e8b815c1388893408a307d01ffef
karllogdat <logdatk@gmail.com>
2025-05-26 12:04:26 +0800

    challenge completed

--- client/signup.php
+++ client/signup.php
@@ -1,61 +1,36 @@
 <?php
+include '../components/connect.php';
 
-include '../components/connect.php'
+// setup form data
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$country = $_POST['country'];
+$pcode = $_POST['postalcode'];
+$email = $_POST['email'];
+$phone = $_POST['telephone'];
+$passnum = $_POST['passportnumber'];
+$passexp = $_POST['passportexpiry'];
+$bdate = $_POST['birthdate'];
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+// insert to passport table firt to ensure passnum exists
+$sql = "INSERT INTO passport (PassportNumber, PassportExpiry, FirstName, LastName, Birthdate) VALUES (?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+try {
+    $stmt->execute([$passnum, $passexp, $fname, $lname, $bdate]);
+} catch (PDOException $e) {
+    die("Error inserting passport into database: " . $e->getMessage());
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign-Up</title>
+// insert to booker table
+$sql = "INSERT INTO booker (PassportNumber, Email, Telephone, Address, PostalCode, City, Country) VALUES (?, ?, ?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+try {
+    $stmt->execute([$passnum, $email, $phone, $address, $pcode, $city, $country]);
+} catch (PDOException $e) {
+    die("Error inserting booker information into database: " . $e->getMessage());
+}
 
-    <link rel="stylesheet" href="../css/signup.css">
-</head>
-
-<body>
-    <div id="bg-img"></div>
-    <div id="content">
-        <form action="" method="post">
-            <h1>Sign-Up</h1>
-
-            <label>
-                First Name
-                <input type="text" name="firstname" placeholder="Juan" required>
-            </label>
-
-            <label>
-                Last Name
-                <input type="text" name="lastname" placeholder="Dela Cruz" required>
-            </label>
-            <br>
-
-            <label>
-                Address
-                <input type="text" name="address" placeholder="#100 Gumamela St., Baranggay Bulaklak" required>
-            </label>
-            <br>
-
-            <label>
-                City
-                <input type="text" name="city" placeholder="Manila" required>
-            </label>
-
-            <label>
-                Country
-                <input type="text" name="country" placeholder="Philippines" required>
-            </label>
-
-            <label>
-                Postal Code
-                <input type="text" name="postalcode" placeholder="1727" required>
-            </label>
-            <br>
-
-            <button type="submit">Register</button>
-        </form>
-    </div>
-</body>
-
-</html>
+header("Location: signup.html");
+exit;
